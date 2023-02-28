509870aa60410dcc77d731aeca03336b2df812ea
GuyJuslain <gkong049@uottawa.ca>
2023-02-27 21:22:27 -0500

    premiere version fonctionnelle

---
+++ script.js
@@ -0,0 +1,73 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+
+let circleTurn 
+
+startGame()
+
+function startGame(){
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true} )
+    })
+    setBoarHoverClass()
+}
+
+function handleClick(e){
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    if (checkWin(currentClass)){
+        console.log('winner')
+    }
+     // Check for Win
+    // check for Draw
+    // Switch Turns
+    swapTurns()
+    setBoarHoverClass()
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
+
+function setBoarHoverClass(){
+
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if(circleTurn) {
+        board.classList.add(CIRCLE_CLASS)
+    }
+    else{
+        board.classList.add(X_CLASS)
+    }
+
+}
+
+function checkWin(currentClass){
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+}
